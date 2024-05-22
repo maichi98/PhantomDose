a383d317253770cda27b099decfc665a396654e0
maichi98 <elaichi.mohammed.1998@gmail.com>
2024-05-22 21:26:37 +0200

    converted patient characteristics script to get_patient_characteristics function

--- phandose/patient/patient_characteristics.py
+++ phandose/patient/patient_characteristics.py
@@ -1,85 +1,90 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 17 14:16:23 2023
-@author: Diallo
-"""
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-import os
-import pandas as pd
+from pathlib import Path
 import pydicom as dcm
-import time
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-start_time = time.time()
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-def is_dicom(file_path):
-    try:
-        dcm.dcmread(file_path)
-        return True
-    except dcm.errors.InvalidDicomError:
-        return False
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-patient_path = os.path.dirname(os.environ.get("PATIENT_PATH"))
-print("-------------gtgt",patient_path)
-Projet = os.path.normpath(patient_path)
-FoldersCT = [x[0] for x in os.walk(Projet) if os.path.basename(x[0])=='CT_TO_TOTALSEGMENTATOR'] 
-FoldersPetCT = [x[0] for x in os.walk(Projet) if os.path.basename(x[0])=='PET_TO_TOTALSEGMENTATOR']
-Folders_CT_PETCT = FoldersCT + FoldersPetCT
-PatientsCharacteristicsDf = pd.DataFrame()
-for folder in Folders_CT_PETCT:
-    print(folder)
-    files_in_folder = os.listdir(folder)
-    First_dcm_file = os.path.join(folder, files_in_folder[0])
-    if (is_dicom(First_dcm_file)) and dcm.dcmread(First_dcm_file).Modality == 'CT':
-        dcm_= dcm.dcmread(First_dcm_file)
-        CodeMeaning = 'nan'        
-        if 'ProcedureCodeSequence' in dcm_:
-            CodeMeaning = dcm_.ProcedureCodeSequence[0].CodeMeaning
-        PatientSize = 'nan'
-        if 'PatientSize' in dcm_:
-            PatientSize = dcm_.PatientSize
-        PatientWeight = 'nan'
-        if 'PatientWeight' in dcm_:
-            PatientWeight = dcm_.PatientWeight
-        AcquisitionDate = 'nan'
-        if 'AcquisitionDate' in dcm_:
-            AcquisitionDate = dcm_.AcquisitionDate
-        DataCollectionDiameter = 'nan'
-        if 'DataCollectionDiameter' in dcm_:
-            DataCollectionDiameter = dcm_.DataCollectionDiameter
-        ReconstructionDiameter = 'nan'
-        if 'ReconstructionDiameter' in dcm_:
-            ReconstructionDiameter = dcm_.ReconstructionDiameter
-        PatientPosition = 'nan'
-        if 'PatientPosition' in dcm_:
-            PatientPosition = dcm_.PatientPosition
-        data = {'Folder': [folder],
-                'Type' : os.path.basename(folder),
-                'PatientID': [dcm_.PatientID],
-                'CodeMeaning': [CodeMeaning],
-                'AcquisitionDate': [AcquisitionDate],
-                'DataCollectionDiameter': [DataCollectionDiameter],
-                'ReconstructionDiameter': [ReconstructionDiameter],
-                'PatientPosition': [PatientPosition],
-                'PatientBirthDate': [dcm_.PatientBirthDate],
-                'PatientSex': [dcm_.PatientSex],
-                'PatientSize': [PatientSize],
-                'PatientWeight': [PatientWeight]}
-        df0 = pd.DataFrame (data, columns = ['Folder',
-                                             'Type',
-                                             'PatientID',
-                                             'CodeMeaning',
-                                             'AcquisitionDate',
-                                             'DataCollectionDiameter',
-                                             'ReconstructionDiameter',
-                                             'PatientPosition',
-                                             'PatientBirthDate',
-                                             'PatientSex',
-                                             'PatientSize',
-                                             'PatientWeight'])
-        PatientsCharacteristicsDf = pd.concat([PatientsCharacteristicsDf,df0], ignore_index=True)
-            
-PatientsCharacteristicsDf.to_pickle(os.path.join(f"{patient_path}\_DFS_\PatientsCharacteristicsDf.pkl"))  
+import pandas as pd
 
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-print("--- %s seconds ---" % (time.time() - start_time))
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def get_patient_characteristics(*list_path_imaging: Path | str) -> pd.DataFrame:
+    """
+    Extract patient characteristics from imaging data (CT or PETCT) stored in specified paths.
+
+    :param list_path_imaging: *(Union[Path, str]) Variable number of paths to imaging Data. (e.g. CT or PETCT)
+    :return: pd.DataFrame, A DataFrame containing the patient characteristics extracted from the specified paths
+
+    """
+
+    cols = ['Folder',
+            'Type',
+            'PatientID',
+            'CodeMeaning',
+            'AcquisitionDate',
+            'DataCollectionDiameter',
+            'ReconstructionDiameter',
+            'PatientPosition',
+            'PatientBirthDate',
+            'PatientSex',
+            'PatientSize',
+            'PatientWeight']
+
+    df_patient_data = pd.DataFrame(columns=cols)
+
+    for path_imaging in list_path_imaging:
+
+        path_imaging = Path(path_imaging)
+        # Get patients characteristics from the first slice :
+        first_slice = next(path_imaging.glob("*.dcm"))
+        # Check that the first slice is a dicom file:
+        if dcm.misc.is_dicom(first_slice):
+            dcm_slice = dcm.dcmread(first_slice)
+            # Extract patients characteristics only if slice modality is CT:
+            if dcm_slice.Modality == "CT":
+                # ID of the patient :
+                patient_id = dcm_slice.get("PatientID", "nan")
+
+                # code meaning of procedure code sequence :
+                code_meaning = dcm_slice.get("ProcedureCodeSequence", None)
+                code_meaning = code_meaning[0].CodeMeaning if code_meaning else 'nan'
+
+                # length or size of the patient in meters :
+                patient_size = dcm_slice.get("PatientSize", "nan")
+
+                # weight of the patient in kilograms :
+                patient_weight = dcm_slice.get("PatientWeight", "nan")
+
+                # the date of the acquisition of the data that resulted in the image :
+                acquisition_date = dcm_slice.get("AcquisitionDate", "nan")
+
+                # the diameter in mm of the region over which data were collected :
+                data_collection_diameter = dcm_slice.get("DataCollectionDiameter", "nan")
+
+                # the diameter in mm of the region from within which data were used
+                # in creating the reconstruction of the image :
+                reconstruction_diameter = dcm_slice.get("ReconstructionDiameter", "nan")
+
+                # patient position descriptor relative to the equipment :
+                patient_position = dcm_slice.get("PatientPosition", "nan")
+
+                # birthdate of the patient :
+                patient_birthdate = dcm_slice.get("PatientBirthDate", "nan")
+
+                # sex of the patient :
+                patient_sex = dcm_slice.get("PatientSex", "nan")
+
+                patient_data = {
+                    "Folder": [str(path_imaging)],
+                    "Type": [path_imaging.stem],
+                    "PatientID": [patient_id],
+                    "CodeMeaning": [code_meaning],
+                    "AcquisitionDate": [acquisition_date],
+                    "DataCollectionDiameter": [data_collection_diameter],
+                    "ReconstructionDiameter": [reconstruction_diameter],
+                    "PatientPosition": [patient_position],
+                    "PatientBirthDate": [patient_birthdate],
+                    "PatientSex": [patient_sex],
+                    "PatientSize": [patient_size],
+                    "PatientWeight": [patient_weight]
+                }
+
+                _df = pd.DataFrame(patient_data)
+                df_patient_data = _df if df_patient_data.empty else pd.concat([df_patient_data, _df], ignore_index=True)
+
+    return df_patient_data
