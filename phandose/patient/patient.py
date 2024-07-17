2ca191505c376174ffb62926f0d7c2a1bf545fc4
maichi98 <elaichi.mohammed.1998@gmail.com>
2024-07-17 09:46:05 +0200

    Composite design pattern for patient

--- phandose/patient/patient.py
+++ phandose/patient/patient.py
@@ -1,29 +1,100 @@
-from phandose.modalities import Modality
+from .study import Study
 
 
 class Patient:
+    """
+    Class representing a patient.
 
-    def __init__(self, id_patient: str, list_modalities: list[Modality] = None):
+    Attributes:
+    -----------
+    patient_id : (str)
+        Patient ID.
 
-        if list_modalities is None:
-            list_modalities = []
+    dict_studies : (dict[str: Study])
+        Dictionary containing the studies of the patient.
 
-        self._id_patient = id_patient
-        self._dict_modalities = {modality.id_modality: modality for modality in list_modalities}
+    Methods:
+    --------
+    add_study(study)
+        Add a study to the patient.
+
+    remove_study(study_instance_uid)
+        Remove a study from the patient.
+
+    get_study(study_instance_uid)
+        Get a study from the patient.
+
+    """
+
+    def __init__(self,
+                 patient_id: str,
+                 list_studies: list[Study] = None):
+
+        """
+        Constructor of the class Patient.
+
+        Parameters:
+        -----------
+        patient_id : (str)
+            Patient ID.
+
+        list_studies : (list[Study]), optional
+            List of studies of the patient
+
+        """
+
+        self._patient_id = patient_id
+        self._dict_studies = {study.study_instance_uid: study
+                              for study in list_studies} if list_studies is not None else {}
 
     @property
-    def id_patient(self):
-        return self._id_patient
+    def patient_id(self):
+        """
+        Getter method for the Patient ID.
+        """
+
+        return self._patient_id
 
     @property
-    def dict_modalities(self):
-        return self._dict_modalities
+    def dict_studies(self):
+        """
+        Getter method for the dictionary of studies.
+        """
 
-    def add_modality(self, modality: Modality):
-        self._dict_modalities[modality.id_modality] = modality
+        return self._dict_studies
 
-    def remove_modality(self, id_modality: str):
-        del self._dict_modalities[id_modality]
+    def add_study(self, study: Study):
+        """
+        Add a study to the patient.
 
-    def get_modality(self, id_modality: str):
-        return self._dict_modalities[id_modality]
+        Parameters:
+        -----------
+        study : (Study)
+            Study to be added.
+        """
+
+        self._dict_studies[study.study_instance_uid] = study
+
+    def remove_study(self, study_instance_uid: str):
+        """
+        Remove a study from the patient.
+
+        Parameters:
+        -----------
+        study_instance_uid : (str)
+            Study Instance UID of the study to be removed
+        """
+
+        self._dict_studies.pop(study_instance_uid)
+
+    def get_study(self, study_instance_uid: str):
+        """
+        Get a study from the patient.
+
+        Parameters:
+        -----------
+        study_instance_uid : (str)
+            Study Instance UID of the study to be retrieved.
+        """
+
+        return self._dict_studies.get(study_instance_uid)
